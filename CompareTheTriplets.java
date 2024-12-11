4d876ddf4513c081f716393e7a32bab524eee237
Kishiannn_ <abudachan122@gmail.com>
2024-12-11 23:38:06 +0800

    Update CompareTheTriplets.java

--- CompareTheTriplets.java
+++ CompareTheTriplets.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+// Its not finish yet //
 
+public class Solution {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      int [] studentA_score = new int[3]; 
+      studentA_score[0] = scan.nextInt();
+      studentA_score[1] = scan.nextInt();
+      studentA_score[2] = scan.nextInt();
+      
+      
+      int [] studentB_score = new int[3]; 
+      studentB_score[0] = scan.nextInt();
+      studentB_score[1] = scan.nextInt();
+      studentB_score[2] = scan.nextInt();
+      
+    }
+}
